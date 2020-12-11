2436b4ffdeaaf9702cfd3a75f5eee29cabad2663
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-12-11 00:03:25 -0300

    first commit

--- cli.py
+++ cli.py
@@ -10,24 +10,12 @@ def create_readme(path: str, project: str):
 
     print('> README.md created!');
 
-def create_repository():
-    print('> Creating repository...');
-    print('> Repository created!');
-
-def commit_and_push():    
-    print('> Commiting and pushing code...');
-    print('> Code committed and pushed!');
     
 @click.command()
 @click.option('--path', '-p')
 @click.option('--project', '-pj')
 def supercommit(path, project='project'):
-    # TODO: Create repository
-    # TODO: Create commit and push to it
-
     create_readme(path, project)
-    create_repository()
-    commit_and_push()
 
 
 # cli.py
--- command.sh
+++ command.sh
@@ -11,9 +11,19 @@ function supercommit() {
     cd && cd ./projects/python/supercommit
     python cli.py --path=$original_path --project=$1
 
+
+    echo '> Initializing repository...'
     git init
+
+    echo '> Adding files...'
     git add .
+    
     git commit -m "first commit"
+    echo '> Code commited!'
+
     hub create $1
+    echo '> Repository created!'
+    
     git push -u origin main
+    echo '> Code committed and pushed!'
 }
